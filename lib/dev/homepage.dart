c44baa1a6d6f851dd0642f7fb629a10e84038304
spatipan <patipan120897@gmail.com>
2021-05-08 22:24:46 +0700

    update Homepage

--- lib/dev/homepage.dart
+++ lib/dev/homepage.dart
@@ -3,40 +3,183 @@ import 'package:flutter/material.dart';
 final Color bgColor = Color.fromARGB(255, 255, 255, 255);
 final Color textColor = Color.fromARGB(255, 0, 0, 0);
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: true,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home page'),
+      title: 'MED CMU Tracker',
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Medical Device Tracker'), /*bottom: TopBar()*/
+          ),
+          body: Center(child: ListBody()),
+          bottomNavigationBar: BottomBar(),
         ),
-        body: Center(child: Text('Hello')),
-        bottomNavigationBar: bottomBar(),
       ),
     );
   }
 }
 
-class bottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBar createState() => _BottomBar();
+}
+
+class _BottomBar extends State<BottomBar> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const TextStyle unselectedstyle = TextStyle(
+    fontSize: 16,
+  );
+  static const TextStyle selectedstyle = TextStyle(
+    fontSize: 20,
+  );
+
+  var bottomNavigationBarItems = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+        icon: Icon(Icons.location_on), label: 'Device location'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.swap_horizontal_circle_rounded),
+        label: 'Borrow/Return',
+        backgroundColor: Color.fromARGB(255, 255, 255, 255)),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.account_circle_rounded), label: 'Profile'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
-      currentIndex: 0,
-      items: [
-        new BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        new BottomNavigationBarItem(
-            icon: Icon(Icons.swap_horizontal_circle_rounded),
-            label: 'Borrow/Return'),
-        new BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'Log out'),
-      ],
+      currentIndex: _selectedIndex,
+      items: bottomNavigationBarItems,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+      iconSize: 40,
+      unselectedLabelStyle: unselectedstyle,
+      selectedLabelStyle: selectedstyle,
+      showUnselectedLabels: false,
+    );
+  }
+}
+
+class TopBar extends StatelessWidget implements PreferredSizeWidget {
+  final tabBarItems = <Tab>[
+    Tab(
+      text: 'All',
+    ),
+    Tab(text: 'U/S'),
+    Tab(text: 'EKG'),
+  ];
+  Size get preferredSize => const Size.fromHeight(100);
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(tabs: tabBarItems);
+  }
+}
+
+class ListSeperate extends StatelessWidget {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+        child: ListView.separated(
+      padding: EdgeInsets.all(8),
+      itemCount: entries.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          child: Text('${entries[index]}'),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    ));
+  }
+}
+
+class ListBody extends StatelessWidget {
+  static const title = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+  static const available = TextStyle(
+    color: Colors.teal,
+    fontSize: 14,
+  );
+  static const busy = TextStyle(
+    color: Colors.deepOrange,
+    fontSize: 14,
+  );
+  static const maintenance = TextStyle(
+    color: Colors.amber,
+    fontSize: 14,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: ListView(padding: EdgeInsets.all(20), children: <Widget>[
+        for (int index = 1; index < 11; index++)
+          Card(
+              child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: SizedBox(
+                    width: 300,
+                    height: 100,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.wifi,
+                        size: 40,
+                      ),
+                      title: Text(
+                        'Ultrasound No.$index',
+                        style: title,
+                      ),
+                      subtitle: Text(
+                        'Available',
+                        style: available,
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    ),
+                  )))
+      ]),
+    );
+  }
+}
+
+class CardTap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          print('Card tapped.');
+        },
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Text(
+            'A card that can be tapped',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
