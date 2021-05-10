d9cef614f8ac834bda61ce697de1608f9c8cdd2a
AuGuSt <august.athisil@gmail.com>
2021-05-10 22:32:52 +0700

    wait fro server date time

---
+++ lib/dev/InUse.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:device_booking/dev/date time.dart';
+
+void main() {
+  runApp(Busy());
+}
+
+//สร้างหัวข้อ+ธีม
+class Busy extends StatelessWidget {
+  static final String title = 'Using device';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(
+      primaryColor: Color.fromARGB(255, 218, 105, 98),
+      scaffoldBackgroundColor: Colors.white,
+    ),
+    home: MainPage(title: title),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  var appBar = AppBar(
+    title: Text(Busy.title, style: TextStyle(fontSize: 28)),
+    centerTitle: true,
+  );
+  Widget build(BuildContext context) => Scaffold(
+    appBar: appBar,
+    body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          height: (MediaQuery.of(context).size.height -
+              appBar.preferredSize.height),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ButtonWidget(
+                    text: 'Report', //สร้างปุ่ม+copy to clipboard
+                    onClicked: () =>
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) => Report(),
+                        )),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: (MediaQuery.of(context).size.height -
+                    appBar.preferredSize.height) /
+                    10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.width / 2,
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: Image.network(
+                          'https://scontent.fcnx4-1.fna.fbcdn.net/v/t1.6435-9/184880727_4162080337208955_872074179837376358_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeFzMXV3_6_c_5A3U4oH_xN_mJBlnkPjqVCYkGWeQ-OpUE_RIzwoH0Cw60DcWaY2sb1aPDxoXxpM5gEwDvLcG_2A&_nc_ohc=QRfxHEdguFgAX-bXfXz&_nc_ht=scontent.fcnx4-1.fna&oh=bf92e661f2eafb8993210e705f1de133&oe=60BF59AF'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        'Duration',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        '$hrs hours $min minutes',
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                        padding: const EdgeInsets.all(25),
+                        child: ButtonWidget(
+                          text: 'คืนอุปกรณ์', //สร้างปุ่ม+copy to clipboard
+                          onClicked: () =>
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) => Back(),
+                              )),
+                        ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )),
+  );
+}
+//buttonwidget
+
+//buttonwidget
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    @required this.text,
+    @required this.onClicked,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => RaisedButton(
+    child: Text(
+      text,
+      style: TextStyle(fontSize: 24),
+    ),
+    shape: StadiumBorder(),
+    color: Theme.of(context).primaryColor,
+    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    textColor: Colors.white,
+    onPressed: onClicked,
+  );
+}
+
+class Report extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: Color.fromARGB(255, 67, 71, 130)),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Reported!', style: Theme.of(context).textTheme.headline4);
+  }
+}
+
+class Back extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: Color.fromARGB(255, 67, 71, 130)),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget2(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Back!', style: Theme.of(context).textTheme.headline4);
+  }
+}
---
+++ lib/dev/date time.dart
@@ -0,0 +1,9 @@
+import 'package:intl/intl.dart';
+
+
+String datetime = "2021-05-10 16:47:43";
+var dateTime1 = DateFormat('yyyy-MM-dd hh:mm:ss').parse(datetime);
+final hrs = DateTime.now().difference(dateTime1).inHours;
+final mins = DateTime.now().difference(dateTime1).inMinutes;
+int min = mins%60;
+
