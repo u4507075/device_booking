0a4e2681d9953520fb3504ae98b37b69816658a7
AuGuSt <august.athisil@gmail.com>
2021-05-10 22:33:52 +0700

    wait fro server date time

--- lib/dev/date time.dart
+++ lib/dev/date time.dart
@@ -1,6 +1,5 @@
 import 'package:intl/intl.dart';
 
-
 String datetime = "2021-05-10 16:47:43";
 var dateTime1 = DateFormat('yyyy-MM-dd hh:mm:ss').parse(datetime);
 final hrs = DateTime.now().difference(dateTime1).inHours;
