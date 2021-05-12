0f5baced08b2f81a232761404ef08c275bee74fe
spatipan <patipan120897@gmail.com>
2021-05-12 14:52:56 +0700

    updated device info page

--- lib/pages/deviceinfo.dart
+++ lib/pages/deviceinfo.dart
@@ -3,6 +3,12 @@ import 'package:device_booking/src/qrfloatingbutton.dart';
 import 'package:flutter/material.dart';
 
 class DeviceInfo extends StatelessWidget {
+  final String _status = 'available';
+  static const TextStyle _titlestyle =
+      TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  static const TextStyle _subtitlestyle =
+      TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,8 +23,33 @@ class DeviceInfo extends StatelessWidget {
                 Container(
                   height: MediaQuery.of(context).size.height * 0.3,
                   decoration: BoxDecoration(color: Colors.grey),
-                  child: IconButton(
-                      icon: Icon(Icons.close, size: 30.0), onPressed: () {}),
+                  // padding: EdgeInsets.all(10.0),
+                  alignment: Alignment.topLeft,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          icon: Icon(Icons.close, size: 30.0),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Report'),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.amber, onPrimary: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ), //TODO add image of the device
                 ),
                 Container(
                   padding: EdgeInsets.all(30.0),
@@ -29,7 +60,16 @@ class DeviceInfo extends StatelessWidget {
                       Table(
                         children: [
                           TableRow(
-                            children: [Text('Status'), Text('Location')],
+                            children: [
+                              Text(
+                                'Status',
+                                style: _titlestyle,
+                              ),
+                              Text(
+                                'Location',
+                                style: _titlestyle,
+                              )
+                            ],
                           ),
                           TableRow(
                             children: [
@@ -40,7 +80,7 @@ class DeviceInfo extends StatelessWidget {
                                     padding: EdgeInsets.all(5.0),
                                     decoration:
                                         BoxDecoration(color: Colors.grey),
-                                    child: Text('Available')),
+                                    child: Text(_status)),
                               ),
                               Padding(
                                 padding: const EdgeInsets.fromLTRB(
@@ -58,25 +98,41 @@ class DeviceInfo extends StatelessWidget {
                       SizedBox(
                         height: 20.0,
                       ),
-                      Text('Last use'),
-                      Container(
-                        padding: EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(color: Colors.grey),
-                        child: Table(
-                          children: [
-                            TableRow(
-                              children: [Text('User'), Text('Intern Patipan')],
-                            ),
-                            TableRow(
-                              children: [Text('Tel'), Text('091-223-2323')],
-                            ),
-                            TableRow(
-                              children: [Text('Date'), Text('12 May 2021')],
-                            ),
-                            TableRow(
-                              children: [Text('Time'), Text('12.00 - 12.15')],
-                            ),
-                          ],
+                      Text('Last use', style: _titlestyle),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Container(
+                          padding: EdgeInsets.all(5.0),
+                          decoration: BoxDecoration(color: Colors.grey),
+                          child: Table(
+                            border: TableBorder(horizontalInside: BorderSide()),
+                            children: [
+                              TableRow(
+                                children: [
+                                  Text('User', style: _subtitlestyle),
+                                  Text('Intern Patipan')
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Text('Tel', style: _subtitlestyle),
+                                  Text('091-223-2323')
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Text('Date', style: _subtitlestyle),
+                                  Text('12 May 2021')
+                                ],
+                              ),
+                              TableRow(
+                                children: [
+                                  Text('Time', style: _subtitlestyle),
+                                  Text('12.00 - 12.15')
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       SizedBox(
