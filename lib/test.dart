973df449302e21831c71179c65ba11da17e4e158
GGainTaHiG <ggaintahig@gmail.com>
2021-05-14 17:52:52 +0700

    streamcontroller success

--- lib/test.dart
+++ lib/test.dart
@@ -49,80 +49,164 @@ class _MyappState extends State<MyApp2> {
         body: ListView(
           children: <Widget>[
             ListTile(
-              //leading: Icon(Icons.map),
-              title: Text('อายุรกรรมชาย 1'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item01);
+                      return Text(selectLo.item01,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.photo_album),
-              title: Text('อายุรกรรมชาย 2'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item02);
+                      return Text(selectLo.item02,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('อายุรกรรมชาย 3'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item03);
+                      return Text(selectLo.item03,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item04);
+                      return Text(selectLo.item04,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item05);
+                      return Text(selectLo.item05,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item06);
+                      return Text(selectLo.item06,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item07);
+                      return Text(selectLo.item07,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item08);
+                      return Text(selectLo.item08,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item09);
+                      return Text(selectLo.item09,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
             ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
-            ),
-            ListTile(
-              //leading: Icon(Icons.phone),
-              title: Text('`other`'),
+              title: StreamBuilder<Object>(
+                  stream: _controller.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot != null &&
+                        snapshot.hasData &&
+                        snapshot.data == "success") {
+                      print(snapshot);
+                      print(snapshot.data);
+                      print(selectLo.item10);
+                      return Text(selectLo.item10,);
+                    } else {
+                      return Text('Loading',);
+                    }
+                  }),
             ),
           ],
         ),
