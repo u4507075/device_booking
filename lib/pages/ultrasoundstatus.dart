295d46224ec635e6fa6ad4ff3aa3bdb2b58d3432
spatipan <patipan120897@gmail.com>
2021-05-16 21:10:19 +0700

    update main

--- lib/pages/ultrasoundstatus.dart
+++ lib/pages/ultrasoundstatus.dart
@@ -6,10 +6,20 @@ import 'package:device_booking/src/button.dart';
 import 'package:device_booking/src/qrfloatingbutton.dart';
 import 'package:device_booking/pages/deviceinfo.dart';
 
-class UltrasoundStatus extends StatelessWidget {
+class UltrasoundStatus extends StatefulWidget {
+  @override
+  _UltrasoundStatusState createState() => _UltrasoundStatusState();
+}
+
+class _UltrasoundStatusState extends State<UltrasoundStatus> {
   final String _assetPath = "assets/images/ultrasonography.png";
-  final String _title = "Ultrasound";
-  final List<Device> devices = Device.fetchAll();
+  List<Device> devices;
+
+  @override
+  void initState() {
+    super.initState();
+    devices = Device.fetchAll();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,23 +53,28 @@ class UltrasoundStatus extends StatelessWidget {
               child: Container(
                 height: MediaQuery.of(context).size.height * 0.7,
                 child: ListView.builder(
-                  // padding: EdgeInsets.all(0.0),
+                  padding: EdgeInsets.all(5.0),
                   itemCount: devices.length,
                   itemBuilder: (context, index) {
                     print(devices[index]);
+                    return deviceStatus(
+                        name: devices[index].name, type: devices[index].type);
                     // return Text(devices[index].name);
+                    // return CardStatus(
+                    //   name: devices[index].name,
+                    //   type: 'ultrasound',
 
-                    return Card(
-                      child: ListTile(
-                        onTap: () {},
-                        title: Text(devices[index].name),
-                        subtitle: Text('Location'),
-                        leading: Image.asset(
-                          _assetPath,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
+                    // return Card(
+                    //   child: ListTile(
+                    //     onTap: () {},
+                    //     title: Text(devices[index].name),
+                    //     subtitle: Text('Location'),
+                    //     leading: Image.asset(
+                    //       _assetPath,
+                    //       fit: BoxFit.cover,
+                    //     ),
+                    //   ),
+                    // );
                   },
                 ),
               ),
@@ -70,5 +85,38 @@ class UltrasoundStatus extends StatelessWidget {
       floatingActionButton: ScanQrCode(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     ));
+  }
+}
+
+Widget deviceStatus({String name, String type}) {
+  String assetPath;
+  if (type == 'ultrasound') {
+    assetPath = "assets/images/ultrasonography.png";
+  } else if (type == 'ekg') {
+    assetPath = "assets/images/electrocardiogram.png";
+  }
+  return CardStatus(name: name, type: type, assetPath: assetPath);
+}
+
+class CardStatus extends StatelessWidget {
+  const CardStatus({Key key, this.name, this.type, this.assetPath})
+      : super(key: key);
+  final String name;
+  final String type;
+  final String assetPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {},
+        title: Text(name),
+        subtitle: Text('Location'),
+        leading: Image.asset(
+          assetPath,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
