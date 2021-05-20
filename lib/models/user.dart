b354ebc9a134163379892ad7bb8158717cd8d282
FearlessCaT <august.athisil@gmail.com>
2021-05-20 14:30:10 +0700

    database.dart update for userdetails

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,85 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
+class UserDetails {
+  String email = '';
+  String firstname = '';
+  String imagePath = '';
+  String lastname = '';
+  String role = '';
+  String telephone = '';
 
-class UserData {
-  String firstname = 'User';
-  String lastname;
-  String phoneNumber;
-  String role;
-  String email;
-  String photoURL;
-  String uid;
 
-  UserData(
-      {this.firstname,
-      this.lastname,
-      this.phoneNumber,
-      this.role,
-      this.email,
-      this.photoURL,
-      this.uid});
+  UserDetails({
+    this.email,
+    this.firstname,
+    this.imagePath,
+    this.lastname,
+    this.role,
+    this.telephone,
+  });
 
-  UserData sample() => UserData(
-        firstname: 'Sample',
-        lastname: 'Sample',
-        phoneNumber: '0981234567',
-        role: 'Intern',
-        email: 'sample@elearning.cmu.ac.th',
-        photoURL:
-            'https://th.jobsdb.com/th-th/cms/employer/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png',
-        uid: 'sdfluioASDFiosdfadsf',
-      );
+  UserDetails defaultValue() =>
+      UserDetails(
+          email: 'unknown',
+          firstname: "unknown",
+          imagePath: "000000" ,
+          lastname: "unknown",
+          role: "unknown",
+          telephone: "0000000000");
 }
-
-//   String firstname;
-//   String lastname;
-//   String phomeNumber;
-//   String role;
-//   String email;
-//   String photoURL;
-//   String uid;
-//   String
-
-//   UserInfo(this.firstname, this.lastname, this.telephone, this.role, this.email,
-//       this.imagePath);
-
-//   static UserInfo fetchAll() {
-//     return UserInfo(
-//       'patipan',
-//       'sitthiprawiat',
-//       '0982518399',
-//       'intern',
-//       'patipan120897@gmail.com',
-//       'https://i.pinimg.com/originals/1a/1d/7f/1a1d7fc83afe89b6ad0f01d075d210f0.png',
-//     );
-//   }
-
-//   static List<User> fetchDetail() {
-//     User data;
-//     data = fetchAll();
-//     // return [''];
-//   }
-// }
-
-// class User {
-//   final String firstname;
-//   final String lastname;
-//   final String imagePath;
-//   final String email;
-//   final String telephone;
-//   final String role;
-
-//   const User({
-//     this.firstname,
-//     this.lastname,
-//     this.imagePath,
-//     this.email,
-//     this.telephone,
-//     this.role,
-//   });
-
-//   static List<User> fetchAll() {
-//     return [
-//       User('First name', 'Lastname')
-//     ];
-//   }
-// }
